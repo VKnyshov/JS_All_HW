9e36d1eaf6fc0f334be5cf2ee5e8fbe22e74bdc2
VKnyshov <valeriy.knyshov@gmail.com>
2024-09-01 12:33:59 +0300

    add all HW

--- HW11/main.js
+++ HW11/main.js
@@ -13,6 +13,9 @@
 //         for (const cart of carts) {
 //             const div = document.createElement('div');
 //             div.classList.add('divClass');
+//             div.style.width = '400px'
+//             div.style.height = '250px'
+//
 //             const nameBasket = document.createElement('p');
 //             nameBasket.innerText = `Number of Basket: ${cart.id}, Total Products: ${cart.totalProducts}`;
 //
@@ -28,18 +31,17 @@
 //                 onePr.style.color = 'yellow';
 //                 allPR.appendChild(onePr);
 //             }
-//
-//             div.append(nameBasket, productInBasket);
-//             document.body.appendChild(div);
-//         }
-//         console.log(document.body.children)
-//     });
+            // div.append(nameBasket, productInBasket);
+            // document.body.appendChild(div);
+        // }
+        // console.log(document.body.children)
+    // });
 //
 
 //     #whXxOBlYS0H
 //     - взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти.
 //     Інгредієнти повинні бути список під час відображення.
-
+//
 fetch('https://dummyjson.com/recipes')
     .then(response => response.json())
     .then(recArr => {
@@ -51,14 +53,8 @@ fetch('https://dummyjson.com/recipes')
             const divRecipe = document.createElement('div');
             divRecipe.style.width = '700px';
             divRecipe.style.height = '1000px';
-
             divRecipe.style.border = '1px solid black';
             divRecipe.style.padding = '20px';
-
-
-
-
-
 
             const h2Name = document.createElement('h2');
             h2Name.innerText = `${recipe.id}. ${recipe.name}`
@@ -71,13 +67,11 @@ fetch('https://dummyjson.com/recipes')
             divHar.style.justifyContent = 'centre';
             divHar.style.alignItems = 'centre';
 
-
             const divHarText = document.createElement('div');
             divHarText.style.padding = '10px';
 
             const caloriesPerServing = document.createElement('p');
             caloriesPerServing.innerText = `Calories Per Serving: ${recipe.caloriesPerServing}`
-
 
             const img = document.createElement('img');
             img.src = recipe.image;
@@ -85,22 +79,17 @@ fetch('https://dummyjson.com/recipes')
             img.style.height = '400px';
             img.style.paddingLeft = '20px';
 
-
-
             const cookTimeMinutes = document.createElement('p');
             cookTimeMinutes.innerText = `Cook Time Minutes: ${recipe.cookTimeMinutes}`
 
             const cuisine = document.createElement('p');
             cuisine.innerText = `Cuisine: ${recipe.cuisine}`
 
-
             const difficulty = document.createElement('p');
             difficulty.innerText = `Difficulty: ${recipe.difficulty}`
 
-
             const mealType = document.createElement('p');
             cookTimeMinutes.innerText = `Meal Type: ${recipe.mealType}`
-
 
             const prepTimeMinutes = document.createElement('p');
             prepTimeMinutes.innerText = `Prep Time Minutes: ${recipe.prepTimeMinutes}`
@@ -108,14 +97,12 @@ fetch('https://dummyjson.com/recipes')
             const rating = document.createElement('p');
             rating.innerText = `Rating: ${recipe.rating}`
 
-
             const reviewCount = document.createElement('p');
             reviewCount.innerText = `Review Count: ${recipe.reviewCount}`
 
 
             const servings = document.createElement('p');
             servings.innerText = `Servings: ${recipe.servings}`
-
 
             const userId = document.createElement('p');
             userId.innerText = `User Id: ${recipe.userId}`
@@ -125,7 +112,6 @@ fetch('https://dummyjson.com/recipes')
 
             const pForUlIngredients = document.createElement('p');
             pForUlIngredients.innerText = 'List of ingredients:';
-
 
             const ulIngredients = document.createElement('ul');
             ulIngredients.style.fontStyle = 'italic';
@@ -137,12 +123,10 @@ fetch('https://dummyjson.com/recipes')
                 divUlIngredients.append(pForUlIngredients, ulIngredients);
             }
 
-
             const ulInstructions = document.createElement('ol');
 
             const cookingMethod = document.createElement('h3');
             cookingMethod.innerText = `Cooking method: `
-
 
             for (const oneInstruction of recipe.instructions) {
                 const liInstructions = document.createElement('li');
@@ -163,14 +147,12 @@ fetch('https://dummyjson.com/recipes')
                ulTagS.appendChild(liTags)
             }
 
-
             divHarText.append(caloriesPerServing, cookTimeMinutes, cuisine, difficulty, mealType, prepTimeMinutes,
                 rating, reviewCount, servings, userId, divUlIngredients)
             divHar.append(img, divHarText)
             divRecipe.append(h2Name, divHar,cookingMethod, ulInstructions, ulTagS )
 
             document.body.appendChild(divRecipe);
-
 
         }
     });
